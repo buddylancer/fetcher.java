09fdac1857794d48c73308b65063da3e1fde0d46
Buddy Lancer <buddy.lancer@gmail.com>
2021-06-14 21:30:37 +0000

    Add copyright notice

--- src/java/Bula/Objects/Logger.java
+++ src/java/Bula/Objects/Logger.java
@@ -1,3 +1,7 @@
+// Buddy Fetcher: simple RSS-fetcher/aggregator.
+// Copyright (c) 2021 Buddy Lancer. All rights reserved.
+// Author - Buddy Lancer <http://www.buddylancer.com>.
+// Licensed under the MIT license.
 
 package Bula.Objects;
 import Bula.Meta;
